8d6911deb8631e25d133e02a494532639c670921
Kundan <kundan kamble>
2022-05-30 02:01:00 -0700

    [KUNDAN] Added Valid First First Name program

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistrationRegex userRegistration = new UserRegistrationRegex();
+            Console.WriteLine("Enter your first Name: ");
+            string input = Console.ReadLine();
+            userRegistration.ValidFirstName(input);
+
+        }
+    }
+}
+
---
+++ UserRegistration/UserRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    internal class UserRegistrationRegex
+    {
+        public Regex FirstName = new Regex(@"^[A-Z][A-Za-z]{2,}$");
+        public void ValidFirstName(string firstName)
+        {
+            Console.WriteLine("First name - " + firstName);
+            if (FirstName.IsMatch(firstName))
+                Console.WriteLine($"{firstName} is valid");
+            else
+                Console.WriteLine($"{firstName} is Invalid");
+
+        }
+
+    }
+}
+
