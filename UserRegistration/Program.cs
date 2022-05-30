2b461389e26c3906e4a5bf1c18218468f71b1977
Kundan <kundan kamble>
2022-05-30 02:10:28 -0700

    [KUNDAN] Added Valid Last Name program

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -7,10 +7,12 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             UserRegistrationRegex userRegistration = new UserRegistrationRegex();
-            Console.WriteLine("Enter your first Name: ");
+            Console.WriteLine("Enter your First Name: ");
             string input = Console.ReadLine();
             userRegistration.ValidFirstName(input);
-
+            Console.WriteLine("Enter your Last Name: ");
+            input = Console.ReadLine();
+            userRegistration.ValidFirstName(input);
         }
     }
 }
--- UserRegistration/UserRegistration.cs
+++ UserRegistration/UserRegistration.cs
@@ -8,6 +8,7 @@ namespace UserRegistration
     internal class UserRegistrationRegex
     {
         public Regex FirstName = new Regex(@"^[A-Z][A-Za-z]{2,}$");
+        public Regex LastName = new Regex(@"^[A-Z][A-Za-z]{2,}$");
         public void ValidFirstName(string firstName)
         {
             Console.WriteLine("First name - " + firstName);
@@ -15,9 +16,16 @@ namespace UserRegistration
                 Console.WriteLine($"{firstName} is valid");
             else
                 Console.WriteLine($"{firstName} is Invalid");
-
         }
 
+        public void ValidLastName(string lastName)
+        {
+            Console.WriteLine("First name - " + lastName);
+            if (FirstName.IsMatch(lastName))
+                Console.WriteLine($"{lastName} is valid");
+            else
+                Console.WriteLine($"{lastName} is Invalid");
+        }
     }
 }
 
